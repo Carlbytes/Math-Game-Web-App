33f05bf69bab0e67644d0a5043f0eb1a9574dc4f
Vermillion321 <aaronquinnmurphyformal@gmail.com>
2025-11-05 14:25:42 +0000

    My work functioning
    Test cases work fine too

--- Math_Game/ConsoleApplication1.cpp
+++ Math_Game/ConsoleApplication1.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <cmath>
+#include "Game.h"
 using namespace std;
 
-// This minigame selected a number at random and then gives you three options. You must pick the option closest to the selected number.
 
+#ifndef RUNNING_TESTS
 int main() {
     srand(static_cast<unsigned int>(time(0))); // Seed random number generator
 
-    int target = rand() % 100 + 1;  // Target number between 1 and 100
-    int options[3];
+    const int MIN_NUMBER = 1;
+    const int MAX_NUMBER = 100;
 
-    // Generate 3 random options (1–100)
-    for (int i = 0; i < 3; ++i) {
-        options[i] = rand() % 100 + 1;
-    }
+    int target = generateRandomNumber(MIN_NUMBER, MAX_NUMBER);
+    int options[3];
+    generateOptions(options, MIN_NUMBER, MAX_NUMBER);
 
     cout << "=== Closest Number Game ===" << endl;
     cout << "Target number: " << target << endl;
@@ -28,23 +27,13 @@ int main() {
     cout << "Enter your choice (1-3): ";
     cin >> choice;
 
-    if (choice < 1 || choice > 3) {
+    if (!isValidChoice(choice)) {
         cout << "Invalid choice!" << endl;
         return 0;
     }
 
     int chosen = options[choice - 1];
-
-    // Find which option is actually closest
-    int closestIndex = 0;
-    int smallestDiff = abs(options[0] - target);
-    for (int i = 1; i < 3; ++i) {
-        int diff = abs(options[i] - target);
-        if (diff < smallestDiff) {
-            smallestDiff = diff;
-            closestIndex = i;
-        }
-    }
+    int closestIndex = findClosestIndex(target, options);
 
     cout << "\nYou chose: " << chosen << endl;
     cout << "The closest number was: " << options[closestIndex] << endl;
@@ -53,8 +42,9 @@ int main() {
         cout << "Correct! You picked the closest number." << endl;
     }
     else {
-        cout << "Not quite. Better luck next time!" << endl;
+        cout << "Incorrect." << endl;
     }
 
     return 0;
 }
+#endif
